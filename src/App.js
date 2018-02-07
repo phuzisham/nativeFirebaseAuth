c2a3dc8ab9e370fd71fa5afa267aa8f1aea490ec
David Hale <david.hale7@gmail.com>
2018-02-07 11:26:00 -0800

    add firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Header } from './Components/Common';
+import firebase from 'firebase';
+import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCIRnj6u_R4jjny9Y1in3QcAhfkHNHsSi8',
+      authDomain: 'nativefirebaseauth.firebaseapp.com',
+      databaseURL: 'https://nativefirebaseauth.firebaseio.com',
+      projectId: 'nativefirebaseauth',
+      storageBucket: 'nativefirebaseauth.appspot.com',
+      messagingSenderId: '527840596899'
+  })
+  }
+
   render() {
     return(
       <View>
